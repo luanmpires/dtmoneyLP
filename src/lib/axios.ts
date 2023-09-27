ef6e04d01422de9c7e5288816e8d77188d36dcb7
Luan Marçal Pires <luan.pires@american.local>
2023-09-27 10:54:21 -0300

    finish project

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: 'http://localhost:3000',
-  // baseURL: 'https://my-json-server.typicode.com/luanmpires/dtmoney',
+  // // baseURL: 'https://my-json-server.typicode.com/luanmpires/dtmoney',
 })
